103f342b748c4fb4484d6eb368e59b90a903b7cd
pinky-banik <pinky.rani.banik@gmail.com>
2022-04-19 01:04:47 +0600

    ok

--- src/Components/Homepage/foods/Foods.js
+++ src/Components/Homepage/foods/Foods.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Foods = () => {
     return (
         <div>
-            <h1>THis is red onion website</h1>
+            <h1>THis is red onion websites</h1>
         </div>
     );
 };
