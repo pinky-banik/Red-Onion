133e5dc6ff846f424b64f4973256024362c9ec6b
pinky-banik <pinky.rani.banik@gmail.com>
2022-04-22 21:30:24 +0600

    ok

--- src/Components/Homepage/Header/Header.js
+++ src/Components/Homepage/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <Navbar></Navbar>
             <div className="flex flex-col items-center pb-56  justify-center h-full">
                 <h1 className="text-center text-3xl md:text-4xl lg:text-5xl  font-semibold text-gray-700 poppins">Best food waiting for your belly</h1>
-
+                {/* abcd */}
                 <div className="rounded-full p-1 box-border mt-8 bg-white overflow-hidden ring-red-300 focus:ring-4 w-96 flex items-center ">
                     <input type="text" className=" rounded-full px-4 focus:outline-none w-full bg-transparent" placeholder="Search here ......." />
                     <button className="btn btn-search">Search</button>
