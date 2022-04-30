b1812e3e9b14deaba4f9b4598b487513f6950160
pinky-banik <pinky.rani.banik@gmail.com>
2022-05-01 00:21:37 +0600

    footer done

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -2,35 +2,39 @@ import React from 'react';
 import logo from "../../../images/logo.png";
 const Footer = () => {
     return (
-        <div className='bg-gray-900 text-white'>
-            <section className='footer'>
-                <div className='main-footer'>
-                <div>
+        <div className='bg-gray-900 text-gray-300 px-6 py-14'>
+            <section className='footer mx-14'>
+                <div className='main-footer flex justify-between '>
+                <div className='w-2/4'>
                     <img className='h-10' src={logo} alt="" />
                 </div>
-                <div>
+                <div className='flex justify-around w-2/4 my-5 leading-8'>
                     <div>
+                        <ul>
                         <li>About Online food</li>
                         <li>Read our blog</li>
                         <li>sign up deliver</li>
                         <li>Add your restaurent</li>
+                        </ul>
                     </div>
                     <div>
+                        <ul>
                         <li>Get help</li>
                         <li>Read FAQs</li>
                         <li>View all cities</li>
                         <li>Restaurents near me</li>
+                        </ul>
                     </div>
                 </div>
                 </div>
-                <div >
-                    <div>
-                        <h1>Developed by 💗 Pinky Banik</h1>
+                <div className='flex mt-14 justify-between'>
+                    <div className='w-1/3'>
+                        <h1 className='text-gray-500'>Developed by 💗 Pinky Banik</h1>
                     </div>
-                    <div>
-                        <h1>privacy policy</h1>
-                        <h1>terms of use</h1>
-                        <h1>pricing</h1>
+                    <div className='flex mr-16'>
+                        <h1 className='mx-10'>privacy policy</h1>
+                        <h1 className='mx-10'>terms of use</h1>
+                        <h1 className='mx-10'>pricing</h1>
                     </div>
                 </div>
             </section>
