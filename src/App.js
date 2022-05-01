7b19d9e5f58dddf64672e71fbac8f431ebe16be1
pinky-banik <pinky.rani.banik@gmail.com>
2022-05-01 13:39:59 +0600

    fooddetails

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Homepage/Home/Home';
@@ -6,6 +5,7 @@ import Login from './Components/Login/Login/Login';
 import Navbar from './Components/Shared/Navbar/Navbar';
 import Register from './Components/Login/Register/Register';
 import Error from './Components/Shared/Error/Error';
+import FoodDetails from './Components/Homepage/foods/FoodDetails';
 
 function App() {
   return (
@@ -13,10 +13,11 @@ function App() {
       <BrowserRouter>
       <Navbar></Navbar>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/signup' element={<Register/>}></Route>
-        <Route path="*" element={<Error/>}></Route>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Register/>}/>
+        <Route path='/foods/:title' element={<FoodDetails/>} />
+        <Route path="*" element={<Error/>}/>
       </Routes>
       </BrowserRouter>
     </div>
