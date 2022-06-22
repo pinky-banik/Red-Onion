0aca14ceca3fc0254c9c8ef1bc392c5adc7ade22
pinky-banik <pinky.rani.banik@gmail.com>
2022-06-23 03:04:29 +0600

    done

--- src/Components/Homepage/Header/Header.js
+++ src/Components/Homepage/Header/Header.js
@@ -9,6 +9,7 @@ const Header = () => {
                 <div className="rounded-full p-1 box-border mt-8 bg-white overflow-hidden ring-red-300 focus:ring-4 w-96 flex items-center ">
                     <input type="text" className=" rounded-full px-4 focus:outline-none w-full bg-transparent" placeholder="Search here ......." />
                     <button className="btn btn-search">Search</button>
+                    {/* done */}
                 </div>
             </div>
         </section> 
